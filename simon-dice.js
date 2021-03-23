0a142f35ba9d25bd47c79e30c8b10f8e4404c276
matiasbobek <matiasbobek@gmail.com>
2021-03-23 16:26:40 -0300

    Agrega funciones correspondientes al primer turno de la máquina
    El juego depende de que la máquina elija un cuadro y sea mostrado, se añadieron las funciones que posibilitan aquello

--- simon-dice.js
+++ simon-dice.js
@@ -34,12 +34,65 @@ TurnoJugador(N){
 
 const $botonEmpezar = document.querySelector("#boton-empezar");
 let turno = 0;
+let esTurnoJugador = false;
+let secuenciaJugador = [];
+let secuenciaMaquina = [];
 
 $botonEmpezar.onclick = function(){
-    turno = 1;
-    mostrarTurno();
+    mostrarTurnoMaquina();
+    $botonEmpezar.disabled = true;
+    //probablemente deshabilitar botones 
 }
 
-function mostrarTurno(turno){
+function mostrarNumeroTurno(){
     const $turno = document.querySelector("#turno").textContent = `Turno: ${turno}`;
+}
+
+function mostrarEstadoJuego(esQuePerdio){
+    const $estado = document.querySelector("#estado")
+
+    if (esQuePerdio){
+        $estado.textContent = `Perdiste en el turno ${turno}`
+    } else if (esTurnoJugador){
+        $estado.textContent = "Es tu turno"
+    } else if (!esTurnoJugador){
+        $estado.textContent = "Es turno de la máquina"
+    }
+}
+
+function mostrarTurnoMaquina(){
+    turno++;
+    mostrarEstadoJuego();
+    mostrarNumeroTurno();
+    const cuadroAleatorio = elegirCuadradoAleatorio();
+    secuenciaMaquina.push(cuadroAleatorio);
+    resaltarCuadro(cuadroAleatorio);   
+    habilitarTurnoJugador();
+}
+
+function elegirCuadradoAleatorio(){
+    const $cuadros = document.querySelectorAll('[name=cuadros]')
+    let numeroAleatorio = Math.floor(Math.random()*$cuadros.length);
+    return $cuadros[numeroAleatorio];
+}
+
+function resaltarCuadro($cuadro){
+    $cuadro.style.opacity = "50%";
+    setTimeout(function(){
+        $cuadro.style.opacity = "100%";
+    },1000)
+}
+
+function habilitarTurnoJugador(){
+    //alertar el cuadro que clickea
+
+}
+
+function habilitarClickearCuadro (){
+    const $cuadros = document.querySelectorAll('[name=cuadros]');
+    $cuadros.forEach(function($cuadro){
+        $cuadro.onclick=function(){
+            alert("me clicearon")
+        }
+    })
 }
